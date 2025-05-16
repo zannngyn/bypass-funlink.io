4d386be48388057494ee5d2f1dfeb8ebdf8c191f
T. Dũng <85283786+phantatdung233@users.noreply.github.com>
2025-05-16 21:49:56 +0700

    Create: tampermonkey.user.js

---
+++ tampermonkey.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Bypass Funlink
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Bypass Funlink
+// @author       xGreen
+// @require      https://raw.githubusercontent.com/phantatdung233/bypass-funlink.io/refs/heads/main/bypass_funlink.js
+// @match        https://funlink.io/*
+// @grant        GM_xmlhttpRequest
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_addStyle
+// @run-at       document-start
+// @icon         https://avatars.githubusercontent.com/u/85283786?s=48&v=4
+// ==/UserScript==
