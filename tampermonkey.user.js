08067db5e3ef7064cfa57201e376fca9e2d03a85
zannngyn <zanngyn@gmail.com>
2025-05-19 01:20:56 +0700

    update req

--- tampermonkey.user.js
+++ tampermonkey.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Bypass Funlink
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Bypass Funlink
 // @author       xGreen
-// @require      https://raw.githubusercontent.com/phantatdung233/bypass-funlink.io/refs/heads/main/bypass_funlink.js
+// @require      https://raw.githubusercontent.com/zanngyn/bypass-funlink.io/refs/heads/main/bypass_funlink.js
 // @match        https://funlink.io/*
 // @grant        GM_xmlhttpRequest
 // @grant        GM_setValue
